5cb946af64701e68e4f7a42512e81f620bbda2c6
lucas mufato <l.mufato@gmail.com>
2021-09-14 01:12:00 -0300

    BDM 2020 - cosas que quedaban 3

---
+++ Base de Datos Masivas/trabajos practicos 0,1 y 2 - Lucas Mufato - 117780/TP02 - Data Warehouse/modelo fisico ej2.sql
@@ -0,0 +1,53 @@
+﻿create table sexo(
+	id_sexo integer not null,
+	descripcion char(10),
+	primary key(id_sexo)
+);
+
+create table condicion(
+	id_condicion integer not null,
+	descripcion varchar(20),
+	primary key(id_condicion)
+);
+
+create table ingreso(
+	id_ingreso integer not null,
+	fecha date,
+	primary key(id_ingreso)
+);
+
+create table carrera(
+	id_carrera integer not null,
+	descripcion char(20),
+	primary key(id_carrera)
+);
+
+create table plan(
+	id_plan integer not null,
+	fk_carrera integer not null,
+	descripcion char(20),
+	
+	primary key(id_plan),
+	foreign key(fk_carrera) references carrera(id_carrera)
+);
+
+create table sede(
+	id_sede integer not null,
+	descripcion char(20),
+	primary key(id_sede)
+);
+
+create table estudiante(
+	fk_sexo integer not null,
+	fk_condicion integer not null,
+	fk_año_ingreso integer not null,
+	fk_plan integer not null,
+	fk_sede integer not null,
+	cantidad integer,
+	
+	foreign key(fk_sexo) references sexo(id_sexo),
+	foreign key(fk_condicion) references condicion(id_condicion),
+	foreign key(fk_año_ingreso) references ingreso(id_ingreso),
+	foreign key(fk_plan) references plan(id_plan),
+	foreign key(fk_sede) references sede(id_sede)	
+);
---
+++ Base de Datos Masivas/trabajos practicos 0,1 y 2 - Lucas Mufato - 117780/TP02 - Data Warehouse/modelo fisico ej3.sql
@@ -0,0 +1,50 @@
+﻿create table cliente(
+	codigo_cliente integer not null,
+	razon_social varchar(20),
+	CUIT varchar(20),
+	saldo_cuenta real,
+	condicion varchar(20),
+	
+	primary key(codigo_cliente)
+);
+
+create table producto(
+	codigo_producto integer not null,
+	descripcion	varchar(100),
+	categoria varchar(20),
+	marca varchar(40),
+	especificaciones varchar(200),
+	precio_unitario real,
+	
+	primary key(codigo_producto)
+);
+
+create table trimestre(
+	id_trimestre integer not null,
+	descripcion varchar(20),
+	
+	primary key(id_trimestre)
+);
+
+create table fecha(
+	id_fecha integer  not null,
+	dia integer,
+	mes integer,
+	año integer,
+	fk_trimestre integer,
+	
+	primary key(id_fecha),
+	foreign key(fk_trimestre) references trimestre(id_trimestre)
+);
+
+create table venta(
+	fk_cliente integer not null,
+	fk_producto integer not null,
+	fk_fecha integer not null,
+	unidades_vendidas integer,
+	monto_total real,
+	
+	foreign key(fk_cliente) references cliente(codigo_cliente),
+	foreign key(fk_producto) references producto(codigo_producto),
+	foreign key(fk_fecha) references fecha(id_fecha)
+);
